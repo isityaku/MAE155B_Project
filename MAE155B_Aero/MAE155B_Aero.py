746cd4a5d8dd6a8ae05ee512fafeb4c2a1a2b7b4
isityaku <166106417+isityaku@users.noreply.github.com>
2024-04-18 16:09:31 -0700

    Add files via upload

--- MAE155B_Aero/MAE155B_Aero.py
+++ MAE155B_Aero/MAE155B_Aero.py
@@ -36,30 +36,40 @@ Re = rho*V_cruise*L/mu # Reynold's number at cruise velocity
 
 ## Airfoil Configuration
 airfoil = "Clark Y" # airfoil used for aircaft wing
-Cl_a = [] # aircraft 2-D lift curve slope
-a0 = [] # angle-of-attack where Cl = 0
-Cl_max = [] # maximum 2-D lift coefficient
-Cl_min = [] # minimum 2-D lift coefficient
-Cd_max = [] # maximum 2-D drag coefficient
-Cd_min = [] # minimum 2-D drag coefficient
+Cl_alp = 0.09756097561 # aircraft 2-D lift curve slope
+alp0 = -3.5 # angle-of-attack where Cl = 0
+Cl_max = 1.5240 # maximum 2-D lift coefficient
+Cl_min = 1.0371 # minimum 2-D lift coefficient
+Cd_max = 0.14464 # maximum 2-D drag coefficient
+Cd_min = 0.00538 # minimum 2-D drag coefficient
 
 ## Aircraft Configuration (refer to MAE155B_Sizing_Scoring.py)
 b = 1.099 # wing span [in m]
 c = 0.182 # wing chord [in m]
+MAC = (2/3)*c # mean aerodynamic chord [in m]
 S = b*c # wing area [in m^2]
 AR = b/c # aspect ratio
-d = [] # fuselage diameter
-S_exp = [] # exposed planform area
-S_ref = [] # reference planform area
-Lambda_quart = [] # sweep angle at quarter-chord
-Lambda_max = [] # max sweep angle
+d = 0.35 # fuselage diameter [in m]
+S_exp_ref = 0.9 # exposed-reference planform area ratio
+Lambda_quart = 0 # sweep angle at quarter-chord
+Lambda_max = 0 # max sweep angle
 
-## Aerodynamic Parameters
-# 3-D Lift
+## 3-D Lift
+# 3-D Lift Calculation
 beta = math.sqrt(1-(M**2))
 F = 1.07*(1+(d/b))**2
-nu = Cl_a/(2*math.pi/beta)
-CL_a = (2*math.pi*AR/(2+math.sqrt(4+((AR**2)*(beta**2)/(nu**2))*(1+(math.tan(math.radians(Lambda_max))/(beta**2)))))*(S_exp/S_ref)*F) # semi-empirical formula for 3-D lift-curve slope
+nu = Cl_alp/(2*math.pi/beta)
+CL_alp = (2*math.pi*AR)/(2+(math.sqrt(4+((AR**2)*(beta**2)/(nu**2))*(1+(math.tan(math.radians(Lambda_max))**2/(beta**2))))))*S_exp_ref*F # semi-empirical formula for 3-D lift-curve slope
 CL_max = 0.9*Cl_max*math.cos(Lambda_quart)
+
+os.system('cls') # clearing terminal
+
+# 3-D Lift Plotting
+alp = np.linspace(0,20,100)
+CL = CL_alp*(alp-alp0)
+print(CL_alp)
+plt.plot(alp, CL)
+plt.ylim(0, CL_max)
+plt.show()
 
 # 3-D Drag
