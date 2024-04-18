2fdafa840ee412f2bf4fc3e0265e4c7a8eef572b
isityaku <166106417+isityaku@users.noreply.github.com>
2024-04-18 00:47:44 -0700

    Add files via upload

---
+++ MAE155B_Aero.py
@@ -0,0 +1,12 @@
+### MAE155B Aerodynamics [WIP]
+# Made By: Lance De La Cruz
+
+## Outline
+# State and justify airfoil choice
+#   Include lift and drag polars for airfoil (XFLR5)
+# Compute parameters for aircraft lift and drag coefficient equations
+#   Refer to lecture for parameters required
+# Define the following aerodynamic parameters (at cruise):
+#   Lift Coefficient
+#   Drag Coefficient
+#   Lift-to-Drag Ratio
---
+++ MAE155B_Prop.py
@@ -0,0 +1,17 @@
+### MAE155B Propulsion [WIP]
+# Made By: Lance De La Cruz
+
+## Outline
+# State and justify motor-prop configuration.
+# Find the following parameters (at cruise):
+#   Efficiency
+#   Thrust
+#   Torque
+#   Power
+#   Advance Ratio
+#   Rotational Speed
+# Plot the following v. advance ratio:
+#   Efficiency
+#   Thrust Coefficient
+#   Torque Coefficient
+#   Power Coefficient
