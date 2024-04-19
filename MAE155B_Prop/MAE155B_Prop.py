485a6d23e8c26ec56b3f5188c3807a3506b2f36e
isityaku <166106417+isityaku@users.noreply.github.com>
2024-04-19 16:27:18 -0700

    Add files via upload

--- MAE155B_Prop/MAE155B_Prop.py
+++ MAE155B_Prop/MAE155B_Prop.py
@@ -24,6 +24,8 @@ import numpy as np
 import pandas as pd
 import prettytable as pt
 
+os.system('cls') # Clearing terminal
+
 ## Atmospheric Parameters
 g = 9.81 # gravitational acceleration [in m/s^2]
 gamma = 1.4 # specific heat ratio
@@ -37,36 +39,36 @@ a = math.sqrt(gamma*R*temp) # speed of sound [in m/s]
 ## Propulsion Configuration (refer to MAE155B_Sizing_Scoring.py)
 # Motor/Prop Configuration
 motor = "Cobra C-2217/16 Brushless Motor"
-prop = "APC 10x7-E"
+prop = "APC 9x6-E"
+prop_data = pd.read_csv("MAE155B_Data/prop_data_6000rpm.csv")
 
 # Configuration Parameters
 B = 2 # number of blades on propeller
-D = 0.254 # outside diameter (from APC website) [in m]
+D = 0.2286 # outside diameter (from APC website) [in m]
 S = math.pi*(D/2)**2 # disk area [in m^2]
 
 ## Propulsion Parameters
 # Cruise Conditions (from APC 10x7-E Simulation Document)
 V_inf = 18 # assumed cruise/freestream velocity [in m/s]
-n = 6000/60 # rotational speed at cruise conditions [in RPS]
-J_cruise = V_inf/(n*D) # advance ratio at cruise conditions
-T_cruise = 1.519 # thrust at cruise conditions [in N]
-Q_cruise = 0.063 # torque at cruise conditions [in N-m]
-PWR_cruise = 39.734 # power at cruise conditions [in W]
-CT_cruise = T_cruise/(rho*(n**2)*(D**4)) # thrust coefficient at cruise conditions
-CQ_cruise = Q_cruise/(rho*(n**2)*(D**5)) # torque coefficient at cruise conditions
-CP_cruise = PWR_cruise/(rho*(n**3)*(D**5)) # power coefficient at cruise conditions
+n_m = 9000 # rotational speed at cruise conditions [in RPM]
+n_s = n_m/60 # rotational speed at cruise conditions [in RPS]
+J_cruise = V_inf/(n_s*D) # advance ratio at cruise conditions
+T_cruise = 4.655 # thrust at cruise conditions [in N]
+Q_cruise = 0.127 # torque at cruise conditions [in N-m]
+PWR_cruise = 119.834 # power at cruise conditions [in W]
+CT_cruise = T_cruise/(rho*(n_s**2)*(D**4)) # thrust coefficient at cruise conditions
+CQ_cruise = Q_cruise/(rho*(n_s**2)*(D**5)) # torque coefficient at cruise conditions
+CP_cruise = PWR_cruise/(rho*(n_s**3)*(D**5)) # power coefficient at cruise conditions
 eta_cruise = CT_cruise*J_cruise/CP_cruise # efficiency at cruise conditions
 
-os.system('cls') # Clearing terminal
-
-print("Below are the propulsion parameters for the motor/propeller configuration at cruise conditions:")
+print("Propulsion Parameters (At Cruise Condition):")
 prop_tab = pt.PrettyTable(["Parameter", "Unit", "Value"])
 prop_tab.add_row(["Motor", "---", motor])
 prop_tab.add_row(["Propeller", "---", prop])
 prop_tab.add_row(["Number of Blades", "---", B])
 prop_tab.add_row(["Outside Diameter", "m", D])
 prop_tab.add_row(["Freestream Velocity", "m/s", V_inf])
-prop_tab.add_row(["Propeller Rotational Velocity", "RPM", n])
+prop_tab.add_row(["Propeller Rotational Velocity", "RPM", n_m])
 prop_tab.add_row(["Advance Ratio", "---", J_cruise])
 prop_tab.add_row(["Thrust", "N", T_cruise])
 prop_tab.add_row(["Torque", "N-m", Q_cruise])
@@ -79,11 +81,10 @@ prop_tab.add_row(["Efficiency", "---", eta_cruise])
 print(prop_tab)
 
 # At variable freestream velocity
-prop_data = pd.read_csv("MAE155B_Data/prop_data_6000rpm.csv")
 J = prop_data['J']
-CT = prop_data['Thrust.1']/(rho*(n**2)*(D**4)) # thrust coefficient
-CQ = prop_data['Torque.1']/(rho*(n**2)*(D**5)) # torque coefficient
-CP = prop_data['PWR.1']/(rho*(n**3)*(D**5)) # power coefficient
+CT = prop_data['Thrust.1']/(rho*(n_s**2)*(D**4)) # thrust coefficient
+CQ = prop_data['Torque.1']/(rho*(n_s**2)*(D**5)) # torque coefficient
+CP = prop_data['PWR.1']/(rho*(n_s**3)*(D**5)) # power coefficient
 eta = CT*J/CP # efficiency
 
 ## Plotting
